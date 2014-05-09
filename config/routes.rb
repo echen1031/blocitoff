4480cf8c01aae94fd49b84c348c405110e2af0b1
Eric Chen <eric87chen@gmail.com>
2014-05-08 23:53:59 -0400

    direct user to lists/index after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Blocitoff::Application.routes.draw do
 
   devise_for :users
+  devise_scope :users do
+    get '/logout' => 'devise/sessions#destroy'
+    get '/login' => 'devise/sessions#new'
+    get '/signout' => 'devise/sessions#destroy'
+    get '/signin' => 'devise/sessions#new'
+    get '/register/' => 'users/registrations#new'
+  end
 
   resources :lists 
   resources :list_items
